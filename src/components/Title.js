e5ae2b99e918651b1cbb32c1acb9aaa88a1feea8
ga111o <ga111o@proton.me>
2023-12-05 13:48:35 +0900

    clock basic func

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,9 +1,35 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Title() {
+    const [currentTime, setCurrentTime] = useState(new Date());
+
+    useEffect(() => {
+        const timerId = setInterval(() => {
+            setCurrentTime(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(timerId);
+        };
+    }, []);
+
+    const addLeadingZero = (num) => num.toString().padStart(2, "0");
+
+    const formattedTime = `${addLeadingZero(
+        currentTime.getHours()
+    )}:${addLeadingZero(currentTime.getMinutes())}:${addLeadingZero(
+        currentTime.getSeconds()
+    )}`;
+
+    const formattedDate = `${currentTime.getFullYear()}.${addLeadingZero(
+        currentTime.getMonth() + 1
+    )}.${addLeadingZero(currentTime.getDate())}`;
+
     return (
         <div>
-            <h1>this is title!</h1>
+            <h1 id="Title">Welcome!</h1>
+            <h1 id="time">{formattedTime}</h1>
+            <p id="date">{formattedDate}</p>
         </div>
     );
 }
