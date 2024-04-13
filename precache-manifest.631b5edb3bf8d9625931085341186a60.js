d8a546296d3c51acf8f00214e36595446edb536b
ga111o <ga111o@proton.me>
2024-04-13 12:46:38 +0900

    Updates

---
+++ precache-manifest.631b5edb3bf8d9625931085341186a60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46ccd1ab9494d18592fd9a32886590ba",
+    "url": "/firefox-startpage/index.html"
+  },
+  {
+    "revision": "47603687554dacb9fd68",
+    "url": "/firefox-startpage/static/css/main.cf947e05.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/firefox-startpage/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "c87ae3f2a437a1b2df8d",
+    "url": "/firefox-startpage/static/js/3.177faaa1.chunk.js"
+  },
+  {
+    "revision": "47603687554dacb9fd68",
+    "url": "/firefox-startpage/static/js/main.d3c693bd.chunk.js"
+  },
+  {
+    "revision": "6b344819036f02404e46",
+    "url": "/firefox-startpage/static/js/runtime~main.22d5f54a.js"
+  },
+  {
+    "revision": "fc04cecc327a13cb54773e5a5fa0597d",
+    "url": "/firefox-startpage/static/media/bg.fc04cecc.png"
+  }
+]);
